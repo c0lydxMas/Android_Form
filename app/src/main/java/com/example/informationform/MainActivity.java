76d7d4b0d45472a9e534d6037f9adc0c5a734c2e
Khanh Bui <quockhanh2609qk@outlook.com.vn>
2021-11-01 16:31:06 +0700

    fix bug

--- app/src/main/java/com/example/informationform/MainActivity.java
+++ app/src/main/java/com/example/informationform/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.informationform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -10,18 +11,24 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
 
-    EditText full_name = (EditText) findViewById(R.id.fullname);
-    EditText student_Id = (EditText) findViewById(R.id.student_id);
-    EditText dob = (EditText) findViewById(R.id.dob);
-    EditText phone = (EditText) findViewById(R.id.phone);
-    EditText email = (EditText) findViewById(R.id.email);
-
+    private EditText full_name;
+    private EditText student_Id;
+    private EditText dob;
+    private EditText phone;
+    private EditText email;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        full_name = findViewById(R.id.fullname);
+        student_Id = findViewById(R.id.student_id);
+        dob = findViewById(R.id.dob);
+        phone = findViewById(R.id.phone);
+        email = findViewById(R.id.email);
+
     }
 
     public void submitHandler(View view) {
@@ -31,9 +38,8 @@ public class MainActivity extends AppCompatActivity {
         String phoneNumber = phone.getText().toString();
         String emailAddress = email.getText().toString();
 
-        if (fullName.matches("") || studentId.matches("") || dateOfBirth.matches("") || phoneNumber.matches("") || emailAddress.matches("")  ) {
+        if (fullName.matches("") || studentId.matches("") || dateOfBirth.matches("") || phoneNumber.matches("") || emailAddress.matches("")) {
             Toast.makeText(this, "Xin vui long dien thong tin vao tat ca nhung truong bat buoc", Toast.LENGTH_SHORT).show();
-            return;
         }
     }
 
